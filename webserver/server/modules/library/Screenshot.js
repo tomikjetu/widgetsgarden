daa97913bfa11b4f9edea9f4c172a9b9971996e7
Tomáš Kordoš <kordo.t12@gmail.com>
2024-04-20 13:09:16 +0200

    Close browser on error

--- webserver/server/modules/library/Screenshot.js
+++ webserver/server/modules/library/Screenshot.js
@@ -15,32 +15,38 @@ export async function takeLibraryScreenshot(widgetId) {
       // no sandbox for linux
       try{
         const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
-        const page = await browser.newPage();
-        await page.goto(`${process.env.API_URL}/widget?widgetId=${widgetId}&previewSecret=${getWidgetPreviewSecret()}`, {timeout: 0});
-        // listen to width and height from console
-        var Matcher = new RegExp("Loaded (?<width>.+)x(?<height>.+)", "g");
-        page.on("console", (msg) => {
-          if (msg.text().includes("Loaded")) {
-            var match = Matcher.exec(msg.text());
-            var width = parseInt(match.groups.width);
-            var height = parseInt(match.groups.height);
-            setTimeout(async () => {
-              await page.screenshot({
-                path: `./server/assets/screenshots/${widgetId}.png`,
-                clip: {
-                  x: 0,
-                  y: 0,
-                  width,
-                  height,
-                },
-              });
-              await browser.close();
-              resolve();
-            }, 5000);
-          }
-        });
+        try{
+          const page = await browser.newPage();
+          await page.goto(`${process.env.API_URL}/widget?widgetId=${widgetId}&previewSecret=${getWidgetPreviewSecret()}`, {waitUntil: 'load'});
+          // listen to width and height from console
+          var Matcher = new RegExp("Loaded (?<width>.+)x(?<height>.+)", "g");
+          page.on("console", (msg) => {
+            if (msg.text().includes("Loaded")) {
+              var match = Matcher.exec(msg.text());
+              var width = parseInt(match.groups.width);
+              var height = parseInt(match.groups.height);
+              setTimeout(async () => {
+                await page.screenshot({
+                  path: `./server/assets/screenshots/${widgetId}.png`,
+                  clip: {
+                    x: 0,
+                    y: 0,
+                    width,
+                    height,
+                  },
+                });
+                await browser.close();
+                resolve();
+              }, 5000);
+            }
+          });
+        }catch(e){
+          browser.close();
+          console.log("Error Taking Screenshot: ", e.message);
+          resolve();
+        }
       }catch(e){
-        console.log("Error Taking Screenshot: ", e.message);
+        console.log("Error Starting browser: ", e.message);
         resolve();
       }
     });
